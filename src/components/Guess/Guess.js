f81efef682b282b76ed5272fa4e13c54ee9af5d6
zayaanw <zayaanw@gmail.com>
2023-09-25 19:33:55 +0100

    Cell component

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,27 +2,27 @@ import React from "react";
 import { range } from "../../utils";
 import { checkGuess } from "../../game-helpers";
 
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : `cell`;
+  return <span className={className}>{letter}</span>;
+}
+
 function Guess({ string, answer }) {
-  const wordle = () => {
-    if (!string) {
-      return range(5).map((_, index) => (
-        <span key={index} className={`cell`}>
-          {undefined}
-        </span>
-      ));
-    } else {
-      const results = checkGuess(string, answer);
-      return results.map(({ letter, status }, index) => (
-        <span key={index} className={`cell ${status}`}>
-          {letter}
-        </span>
-      ));
-    }
-  };
+  const results = checkGuess(string, answer);
+
+  console.log({ results });
 
   return (
     <>
-      <p className="guess">{wordle()}</p>
+      <p className="guess">
+        {range(5).map((num) => (
+          <Cell
+            key={num}
+            letter={results ? results[num].letter : undefined}
+            status={results ? results[num].status : undefined}
+          />
+        ))}
+      </p>
     </>
   );
 }
